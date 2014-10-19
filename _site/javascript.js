9c5f8f9c684a91aebeafd0c3fc636dc7641c253d
Gianluca Tranchedone <gianluca@cocoabeans.me>
2014-10-19 20:50:15 +0100

    Moving to new layout

--- _site/javascript.js
+++ _site/javascript.js
@@ -15,4 +15,15 @@ $(document).ready(function() {
 			}, 320);
 		}
 	});
+	
+	$('a[href^="#"]').on('click', function(e) {
+	    e.preventDefault();
+	    var target = this.hash;
+	    $target = $(target);
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 });
