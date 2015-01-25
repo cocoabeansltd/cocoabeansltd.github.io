08f8c588aa363799c3dbf3ad1b870ed92a6c0fa3
Gianluca Tranchedone <gianluca@cocoabeans.io>
2015-01-25 20:02:12 +0000

    Improvements

--- _site/javascript.js
+++ _site/javascript.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
 	    $('html, body').stop().animate({
 	        'scrollTop': $target.offset().top - 80
 	    }, 900, 'swing', function () {
-	        window.location.hash = target;
+		    // do nothing
 	    });
 	});
 });
